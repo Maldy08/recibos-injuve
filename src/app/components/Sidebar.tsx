ccbf554aa0f8b28028ff1c20a3c89696c1130ff1
Aguero224 <carlos.aguero2002@hotmail.com>
2024-08-21 10:09:10 -0700

    version 1.2 del proyecto

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,38 +1,104 @@
-import Link from "next/link"
-import { IoDocumentAttachOutline, IoHomeOutline } from "react-icons/io5"
+'use client';
 
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { IoDocumentAttachOutline, IoHomeOutline, IoMenuOutline, IoCloseOutline, IoArrowUp, IoArrowDown, IoPencil } from 'react-icons/io5';
 
 export const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 1024) {
+                setIsOpen(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
-            <aside id="default-sidebar" className=" top-0 left-0 w-60 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-200 dark:bg-gray-800">
-                    <ul className="space-y-2 font-medium">
-                        <li>
-                            <Link href={'/transparencia'} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition-all">
-                                <IoHomeOutline className=" w-6 h-6 text-gray-800 " />
-                                <span className="flex-1 ml-1 whitespace-nowrap  text-gray-500 ">Home</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/transparencia/documentos'} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition-all">
-                                <IoDocumentAttachOutline className=" w-6 h-6 text-gray-800 " />
-                                <span className="flex-1 ml-1 whitespace-nowrap  text-gray-500">Documentos</span>
-                            </Link>
-                        </li>
-                        {/* <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-                                </svg>
-                                <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-                            </a>
-                        </li> */}
+            {/* Botón para abrir el Sidebar en dispositivos móviles */}
+            {!isOpen && (
+                <button 
+                    onClick={() => setIsOpen(true)} 
+                    className="lg:hidden fixed top-24 left-4 z-50 bg-green-500 text-white p-3 rounded-full shadow-lg transition-transform transform hover:bg-green-600"
+                >
+                    <IoMenuOutline className="w-6 h-6" />
+                </button>
+            )}
 
-                    </ul>
+            {/* Sidebar */}
+            <aside 
+                id="default-sidebar" 
+                className={`fixed top-0 left-0 w-64 h-[calc(100vh+50px)] bg-green-800 text-white transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 lg:relative lg:w-64 z-40`} 
+                aria-label="Sidebar"
+            >
+                <div className="relative h-full flex flex-col">
+                    {/* Imagen del logo en la parte superior del menú, solo visible en pantallas grandes */}
+                    <div className="hidden lg:block">
+                        <Image 
+                            src="/assets/logo-blanco.png" 
+                            alt="logo" 
+                            width={225} 
+                            height={100} 
+                            className="w-full h-auto object-cover" 
+                        />
+                    </div>
+
+                    {/* Botón de cerrar el menú en la vista móvil */}
+                    {isOpen && (
+                        <button 
+                            onClick={() => setIsOpen(false)} 
+                            className="absolute top-4 right-4 bg-red-500 text-white p-2 rounded-full shadow-lg transition-transform transform hover:bg-red-600 lg:hidden z-50"
+                        >
+                            <IoCloseOutline className="w-6 h-6" />
+                        </button>
+                    )}
+
+                    <div className="flex-grow overflow-y-auto px-4 py-6">
+                        <ul className="space-y-4 font-medium">
+                            <li>
+                                <Link href={'/transparencia'} className="flex items-center p-3 rounded-lg transition-transform duration-300 lg:hover:scale-105 lg:hover:bg-green-700">
+                                    <IoHomeOutline className="w-6 h-6" />
+                                    <span className="ml-4 text-lg">Home</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={'/transparencia/documentos'} className="flex items-center p-3 rounded-lg transition-transform duration-300 lg:hover:scale-105 lg:hover:bg-green-700">
+                                    <IoDocumentAttachOutline className="w-6 h-6" />
+                                    <span className="ml-4 text-lg">Documentos</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/transparencia/"} className="flex items-center p-3 rounded-lg transition-transform duration-300 lg:hover:scale-105 lg:hover:bg-green-700">
+                                    <IoArrowUp className="w-6 h-6" />
+                                    <span className="ml-4 text-lg">Oficios Recibidos</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/transparencia/"} className="flex items-center p-3 rounded-lg transition-transform duration-300 lg:hover:scale-105 lg:hover:bg-green-700">
+                                    <IoArrowDown className="w-6 h-6" />
+                                    <span className="ml-4 text-lg">Oficios Expedidos</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/transparencia/"} className="flex items-center p-3 rounded-lg transition-transform duration-300 lg:hover:scale-105 lg:hover:bg-green-700">
+                                    <IoPencil className="w-6 h-6" />
+                                    <span className="ml-4 text-lg">Oficios por Expedir</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </aside>
         </>
-    )
-}
-//IoDocumentAttachOutline
+    );
+};
--- src/app/oficios/layout.tsx
+++ src/app/oficios/layout.tsx
@@ -1,39 +1,33 @@
-import { getServerSession } from "next-auth";
-import { Footer, Header } from "../components";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { Footer, Header, Sidebar } from "../components";
 
-
-export default async function TransparenciaLayout({
+export default async function OficiosLayout({
     children
 }: {
     children: React.ReactNode;
 }) {
 
-    
   const session = await getServerSession(authOptions);
   if( !session ) {
     redirect('/api/auth/signin')
   }
-    return (
-        <div className="flex h-screen ">
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <Header />
-                <div className="flex h-full">
-                    {/* <div className="flex w-64 h-full">
-                        <div className="w-full flex mx-auto ">
-                            <Sidebar />
-                        </div>
-                    </div> */}
-                    <main className="flex grow  overflow-x-hidden overflow-y-auto mb-14">
-                        <div className=" w-full px-6 py-8 lg:md:items-center lg:md:justify-center">
-                            { children }
-                        </div>
-                    </main>
-                </div>
-            </div>
-            <Footer />
-        </div>
 
-    );
+  return (
+    <div className="flex h-screen">
+      <Sidebar /> {/* Mueve el sidebar fuera del flex principal para asegurarte de que ocupe su espacio adecuado */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+        <div className="flex h-full ml-4 mt-16"> {/* Añade margen izquierdo para el sidebar y superior para el header */}
+          <main className="flex-grow overflow-x-hidden overflow-y-auto mb-14">
+            <div className="w-full px-6 py-8 lg:md:items-center lg:md:justify-center">
+              { children }
+            </div>
+          </main>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
