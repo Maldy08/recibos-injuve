503a06c6ecaf65046864add0fd78706c918f8b58
Carlos Maldonado <32904610+Maldy08@users.noreply.github.com>
2025-05-07 22:16:35 -0700

    nada

--- src/app/api/oficios-api.ts
+++ src/app/api/oficios-api.ts
@@ -6,7 +6,7 @@ const oficiosApi = axios.create({
      // baseURL: 'http://200.56.97.5:7282/api-transparencia',
    //
    //  baseURL:'http://200.56.97.5:7281/',
-   baseURL: 'http://3.133.142.151:3001/'
+   baseURL: 'http://localhost:3001/'
     
 });
 
